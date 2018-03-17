fb9de2a7f0d85104788804adf0f540777af27d23
Rey Tucker <git@ryantucker.us>
2018-03-17 23:27:10 +0000

    local: nginx: clean up headers a bit?

--- local/update.sh
+++ local/update.sh
@@ -2,11 +2,12 @@
 
 cd `dirname $0`
 
-cp /etc/nginx/sites-available/vulpine.club nginx/vulpine.club
-cp /etc/nginx/ssl-include.conf nginx/ssl-include.conf
+cp /etc/nginx/sites-available/vulpine.club nginx/
+cp /etc/nginx/ssl-include.conf nginx/
+cp /etc/nginx/vulpine-headers-include.conf nginx/
 
-cp /etc/cron.daily/certbot cron.daily/certbot
-cp /etc/cron.daily/mastodon cron.daily/mastodon
+cp /etc/cron.daily/certbot cron.daily/
+cp /etc/cron.daily/mastodon cron.daily/
 
-cp /etc/cron.hourly/mastodon cron.hourly/mastodon
+cp /etc/cron.hourly/mastodon cron.hourly/
 
