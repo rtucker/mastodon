3df1cfd4932fea87e83a55ec193c6880ef1da7bd
Rey Tucker <git@reytucker.us>
2018-03-20 12:53:35 -0400

    app/glitch: update attribution to refer to Glitchsoc (Vulpine Club remix)
    We are technically a fork, so...

--- app/javascript/flavours/glitch/features/getting_started/index.js
+++ app/javascript/flavours/glitch/features/getting_started/index.js
@@ -172,9 +172,10 @@ export default class GettingStarted extends ImmutablePureComponent {
               <p>
                 <FormattedMessage
                   id='getting_started.open_source_notice'
-                  defaultMessage='Glitchsoc is open source software, a friendly fork of {Mastodon}. You can contribute or report issues on GitHub at {github}.'
+                  defaultMessage='{Glitchsoc} (Vulpine Club remix) is open source software, a friendly fork of {Mastodon}. You can contribute or report issues on GitHub at {github}.'
                   values={{
-                    github: <a href='https://github.com/glitch-soc/mastodon' rel='noopener' target='_blank'>glitch-soc/mastodon</a>,
+                    Glitchsoc: <a href='https://github.com/glitch-soc/mastodon' rel='noopener' target='_blank'>glitch-soc/mastodon</a>,
+                    github: <a href='https://github.com/vulpineclub/mastodon' rel='noopener' target='_blank'>vulpineclub/mastodon</a>,
                     Mastodon: <a href='https://github.com/tootsuite/mastodon' rel='noopener' target='_blank'>Mastodon</a>,
                   }}
                 />
