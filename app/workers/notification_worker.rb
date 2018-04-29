bbc290bb4979fb4fe2fdc3ca7a0f825392c7981e
Rey Tucker <git@reytucker.us>
2018-04-29 00:29:12 -0400

    add stoplight to notification_worker

--- app/workers/notification_worker.rb
+++ app/workers/notification_worker.rb
@@ -6,6 +6,23 @@ class NotificationWorker
   sidekiq_options queue: 'push', retry: 5
 
   def perform(xml, source_account_id, target_account_id)
-    SendInteractionService.new.call(xml, Account.find(source_account_id), Account.find(target_account_id))
+    @xml = xml
+    @source_account = Account.find(source_account_id)
+    @target_account = Account.find(target_account_id)
+
+    process_notification
+
+  rescue => e
+    raise e.class, "Notification failed for #{source_account_id} to #{target_account_id}: #{e.message}", e.backtrace[0]
+  end
+
+  private
+
+  def process_notification
+    light = Stoplight(@target_account.domain) do
+      SendInteractionService.new.call(xml, @source_account, @target_account)
+    end
+
+    light.run
   end
 end
