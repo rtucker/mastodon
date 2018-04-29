9da59a43ad402ed725a4d0f63797d56c02c979a9
Rey Tucker <git@reytucker.us>
2018-04-29 15:11:51 -0400

    add Stoplight to ThreadResolveWorker

--- app/workers/thread_resolve_worker.rb
+++ app/workers/thread_resolve_worker.rb
@@ -10,12 +10,27 @@ class ThreadResolveWorker
   end
 
   def perform(child_status_id, parent_url)
-    child_status  = Status.find(child_status_id)
-    parent_status = FetchRemoteStatusService.new.call(parent_url)
+    @child_status_id = child_status_id
+    @parent_url = parent_url
+    @host = Addressable::URI.parse(parent_url).normalized_host
 
-    return if parent_status.nil?
-
-    child_status.thread = parent_status
-    child_status.save!
+    process_resolve
   end
+
+  private
+
+  def process_resolve
+    light = Stoplight(@host) do
+      child_status  = Status.find(@child_status_id)
+      parent_status = FetchRemoteStatusService.new.call(@parent_url)
+
+      return if parent_status.nil?
+
+      child_status.thread = parent_status
+      child_status.save!
+    end
+
+    light.run
+  end
+
 end
