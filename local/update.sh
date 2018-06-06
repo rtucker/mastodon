e7a455f39d971d64ab8ef27e6be0714163ae1482
Rey Tucker <git@ryantucker.us>
2018-06-06 21:35:09 +0000

    local: fixup expiration of database backups
    move the cleanup find to its own cron job, run hourly

--- local/update.sh
+++ local/update.sh
@@ -10,6 +10,7 @@ cp /etc/cron.daily/certbot cron.daily/
 cp /etc/cron.daily/mastodon cron.daily/
 
 cp /etc/cron.hourly/mastodon cron.hourly/
+cp /etc/cron.hourly/cleanup-backups cron.hourly/
 
 mkdir -p ../public/assets/
 curl http://127.0.0.1:3000/assets/500.html > ../public/assets/500.html
