eb3ffb74d560f2c23fa4ffe876432226614a593b
Rey Tucker <git@reytucker.us>
2018-12-02 23:12:13 +0000

    local: add logrotate script

--- local/update.sh
+++ local/update.sh
@@ -5,6 +5,7 @@ cd `dirname $0`
 cp /etc/nginx/sites-available/vulpine.club nginx/
 cp /etc/nginx/ssl-include.conf nginx/
 cp /etc/nginx/vulpine-headers-include.conf nginx/
+cp /etc/logrotate.d/nginx nginx/logrotate-nginx
 
 cp /etc/cron.daily/certbot cron.daily/
 cp /etc/cron.daily/mastodon cron.daily/
