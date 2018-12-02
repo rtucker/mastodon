f119ef489c488b20bae2f4f7f48b64d100a8878e
Thibaut Girka <thib@sitedethib.com>
2018-12-02 11:43:49 +0100

    [Glitch] Adds follow action timestamp to notification
    Port 330e320b40e975472e042730f9dfb23083f170e3 to glitch-soc

--- app/javascript/flavours/glitch/features/notifications/components/follow.js
+++ app/javascript/flavours/glitch/features/notifications/components/follow.js
@@ -81,11 +81,13 @@ export default class NotificationFollow extends ImmutablePureComponent {
               <i className='fa fa-fw fa-user-plus' />
             </div>
 
-            <FormattedMessage
-              id='notification.follow'
-              defaultMessage='{name} followed you'
-              values={{ name: link }}
-            />
+            <span title={notification.get('created_at')}>
+              <FormattedMessage
+                id='notification.follow'
+                defaultMessage='{name} followed you'
+                values={{ name: link }}
+              />
+            </span>
           </div>
 
           <AccountContainer hidden={hidden} id={account.get('id')} withNote={false} />
