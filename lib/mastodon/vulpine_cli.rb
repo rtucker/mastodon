b952c8507f6a9468754bc2d62a46ccbeb63db102
Rey Tucker <git@reytucker.us>
2018-12-17 00:23:26 -0500

    local: tootctl/vulpine: add skdomains command

--- lib/mastodon/vulpine_cli.rb
+++ lib/mastodon/vulpine_cli.rb
@@ -91,5 +91,45 @@ module Mastodon
         say
       end
     end
+
+    desc 'skdomains', 'List instances with retries, and how many'
+    def skdomains
+      rs = Sidekiq::RetrySet.new
+
+      jobs = rs.select {|j| j.item['class'] == 'ActivityPub::DeliveryWorker'}
+
+      say("AP::DeliveryWorker count: #{jobs.size}, out of #{rs.size}")
+
+      reds = []
+      greens = []
+
+      jobs.map {|j| j.item['args'][2]}.uniq.each do |inbox|
+        inbox_jobs = jobs.select {|x| x.item['args'][2] == "#{inbox}"}
+
+        light = Stoplight(inbox)
+
+        if color(light.color) == :green
+          greens << inbox_jobs.size
+        else
+          reds << inbox_jobs.size
+        end
+
+        say("#{inbox}: job count #{inbox_jobs.size}, color #{light.color}", color(light.color))
+      end
+
+      say
+      say("Red domains: #{reds.size}, total job count: #{reds.sum}")
+      say("Green domains: #{greens.size}, total job count: #{greens.sum}")
+    end
+
+    private
+
+    def color(str)
+      if str == "green"
+        :green
+      else
+        :red
+      end
+    end
   end
 end
