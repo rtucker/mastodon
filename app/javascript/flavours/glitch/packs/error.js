072bf63e84f4679628a7ee13b7b9f9cbd4b4c62d
Rey Tucker <git@reytucker.us>
2019-04-02 10:58:20 -0400

    local: error.js: don't use oops.gif at all

--- app/javascript/flavours/glitch/packs/error.js
+++ app/javascript/flavours/glitch/packs/error.js
@@ -1,13 +1,13 @@
 import ready from 'flavours/glitch/util/ready';
 
 ready(() => {
-  const image = document.querySelector('img');
+  // const image = document.querySelector('img');
 
-  image.addEventListener('mouseenter', () => {
-    image.src = '/oops.gif';
-  });
+  // image.addEventListener('mouseenter', () => {
+  //   image.src = '/oops.gif';
+  // });
 
-  image.addEventListener('mouseleave', () => {
-    image.src = '/oops.png';
-  });
+  // image.addEventListener('mouseleave', () => {
+  //   image.src = '/oops.png';
+  // });
 });
