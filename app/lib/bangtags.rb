05ed9b6ceadf4aba4e71c84e5b8df0985e1bee4e
multiple creatures <dev@multiple-creature.party>
2019-04-22 19:30:54 -0500

    `i:am`: replace horizontal rule with ZWNJ.

--- app/lib/bangtags.rb
+++ app/lib/bangtags.rb
@@ -421,7 +421,7 @@ class Bangtags
         description = @vars["_they:are:#{name}"]
         next if description.blank? || @chunks.last.starts_with?('—')
         status.local_only = true if Status::LOCAL_ONLY_TOKENS.match?(@chunks.last)
-        @chunks << "\n\n---\n— #{description}"
+        @chunks << "\n\n\u200c\n— #{description}"
       when 'media'
         media_idx = post_cmd[1]
         media_cmd = post_cmd[2]
