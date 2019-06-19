fbbbc9b81ae6ff8ca97185abcb36e93a323e65d8
Thibaut Girka <thib@sitedethib.com>
2019-06-19 15:41:17 +0200

    port glitch-soc@`38d2882` to monsterfork: Fix NavigationBar styling

--- app/javascript/flavours/glitch/features/compose/components/navigation_bar.js
+++ app/javascript/flavours/glitch/features/compose/components/navigation_bar.js
@@ -20,20 +20,22 @@ export default class NavigationBar extends ImmutablePureComponent {
           <Avatar account={this.props.account} size={48} />
         </Permalink>
 
-        <Permalink className='acct' href={this.props.account.get('url')} to={`/accounts/${this.props.account.get('id')}`}>
-          <strong>@{this.props.account.get('acct')}</strong>
-        </Permalink>
+        <div className='navigation-bar__profile'>
+          <Permalink className='acct' href={this.props.account.get('url')} to={`/accounts/${this.props.account.get('id')}`}>
+            <strong>@{this.props.account.get('acct')}</strong>
+          </Permalink>
 
-        { profileLink !== undefined && (
-          <a
-            className='edit'
-            href={ profileLink }
-          ><FormattedMessage id='navigation_bar.edit_profile' defaultMessage='Edit profile' /></a>
-        )}
+          { profileLink !== undefined && (
+            <a
+              className='edit'
+              href={ profileLink }
+            ><FormattedMessage id='navigation_bar.edit_profile' defaultMessage='Edit profile' /></a>
+          )}
 
-        <Permalink className='drafts' href='/tags/self.draft' to='/timelines/tag/self.draft'>
-          <FormattedMessage id='navigation_bar.drafts' defaultMessage='Show drafts' />
-        </Permalink>
+          <Permalink className='drafts' href='/tags/self.draft' to='/timelines/tag/self.draft'>
+            <FormattedMessage id='navigation_bar.drafts' defaultMessage='Show drafts' />
+          </Permalink>
+        </div>
       </div>
     );
   };
