6f1a07945ee097e5e57f99cbc9b4e5d57e32d79d
multiple creatures <dev@multiple-creature.party>
2019-07-15 14:44:36 -0500

    make sure user exists before migrating

--- db/migrate/20190531082452_move_account_vars_to_users.rb
+++ db/migrate/20190531082452_move_account_vars_to_users.rb
@@ -1,5 +1,10 @@
 class MoveAccountVarsToUsers < ActiveRecord::Migration[5.2]
   def up
-    Account.local.find_each { |a| a.user.vars = a.vars; a.user.save }
+    Account.local.find_each do |a|
+      if a.user.present?
+        a.user.vars = a.vars
+        a.user.save
+      end
+    end
   end
 end
