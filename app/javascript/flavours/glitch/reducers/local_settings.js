dc32d286bd461447ea09cdb5313d86a3e7bbbdb8
multiple creatures <dev@multiple-creature.party>
2019-07-19 15:46:24 -0500

    use high-vis privacy icons by default

--- app/javascript/flavours/glitch/reducers/local_settings.js
+++ app/javascript/flavours/glitch/reducers/local_settings.js
@@ -18,7 +18,7 @@ const initialState = ImmutableMap({
   confirm_before_clearing_draft: true,
   preselect_on_reply: true,
   inline_preview_cards: true,
-  hicolor_privacy_icons: false,
+  hicolor_privacy_icons: true,
   show_content_type_choice: true,
   content_warnings : ImmutableMap({
     auto_unfold : false,
