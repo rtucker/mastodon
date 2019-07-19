cfd314432d14b95e79ea7367197fdc69de05c5b2
multiple creatures <dev@multiple-creature.party>
2019-07-19 08:15:22 -0500

    correct references in kobold migration

--- db/migrate/20190719121326_migrate_user_kobolds_to_account.rb
+++ db/migrate/20190719121326_migrate_user_kobolds_to_account.rb
@@ -2,8 +2,9 @@ class MigrateUserKoboldsToAccount < ActiveRecord::Migration[5.2]
   disable_ddl_transaction!
   def up
     Account.local.find_each do |account|
-      account.kobold = a.user_is_a_kobold? || false
-      account.gently = a.user_gentlies_kobolds? || false
+      next if account.user.nil?
+      account.kobold = account.user.settings.is_a_kobold? || false
+      account.gently = account.user.settings.gentlies_kobolds? || false
       account.save
     end
   end
