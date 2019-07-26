e0b257d512708c8c4fc14af538501954070e0056
multiple creatures <dev@multiple-creature.party>
2019-07-26 12:42:39 -0500

    remove hashtag visibility warning - this fork supports unlisted hashtags

--- app/javascript/flavours/glitch/features/compose/containers/warning_container.js
+++ app/javascript/flavours/glitch/features/compose/containers/warning_container.js
@@ -5,21 +5,14 @@ import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { me } from 'flavours/glitch/util/initial_state';
 
-const APPROX_HASHTAG_RE = /(?:^|[^\/\)\w])#(\w*[a-zA-Z·]\w*)/i;
-
 const mapStateToProps = state => ({
   needsLockWarning: state.getIn(['compose', 'privacy']) === 'private' && !state.getIn(['accounts', me, 'locked']),
-  hashtagWarning: state.getIn(['compose', 'privacy']) !== 'public' && APPROX_HASHTAG_RE.test(state.getIn(['compose', 'text'])),
   directMessageWarning: state.getIn(['compose', 'privacy']) === 'direct',
 });
 
-const WarningWrapper = ({ needsLockWarning, hashtagWarning, directMessageWarning }) => {
+const WarningWrapper = ({ needsLockWarning, directMessageWarning }) => {
   if (needsLockWarning) {
     return <Warning message={<FormattedMessage id='compose_form.lock_disclaimer' defaultMessage='Your account is not {locked}. Anyone can follow you to view your follower-only posts.' values={{ locked: <a href='/settings/profile'><FormattedMessage id='compose_form.lock_disclaimer.lock' defaultMessage='locked' /></a> }} />} />;
-  }
-
-  if (hashtagWarning) {
-    return <Warning message={<FormattedMessage id='compose_form.hashtag_warning' defaultMessage="This toot won't be listed under any hashtag as it is unlisted. Only public toots can be searched by hashtag." />} />;
   }
 
   if (directMessageWarning) {
@@ -37,7 +30,6 @@ const WarningWrapper = ({ needsLockWarning, hashtagWarning, directMessageWarning
 
 WarningWrapper.propTypes = {
   needsLockWarning: PropTypes.bool,
-  hashtagWarning: PropTypes.bool,
   directMessageWarning: PropTypes.bool,
 };
 
