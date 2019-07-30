d51a846d3a1383801711d41740360f8152535395
multiple creatures <dev@multiple-creature.party>
2019-07-30 13:37:33 -0500

    revert to `text/plain` for transparancy logger

--- app/workers/log_worker.rb
+++ app/workers/log_worker.rb
@@ -22,7 +22,7 @@ class LogWorker
       tags: [tag],
       visibility: :unlisted,
       local_only: true,
-      content_type: 'text/console',
+      content_type: 'text/plain',
       language: 'en',
       nocrawl: true,
       nomentions: true,
