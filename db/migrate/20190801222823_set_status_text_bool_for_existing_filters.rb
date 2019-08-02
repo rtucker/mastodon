7cfc0f0dced56dd2d425ffe7151f5aa5dc0d79eb
multiple creatures <dev@multiple-creature.party>
2019-08-02 02:32:22 -0500

    fix name of `spoiler` column in migration

--- db/migrate/20190801222823_set_status_text_bool_for_existing_filters.rb
+++ db/migrate/20190801222823_set_status_text_bool_for_existing_filters.rb
@@ -1,6 +1,6 @@
 class SetStatusTextBoolForExistingFilters < ActiveRecord::Migration[5.2]
   def up
     CustomFilter.where(status_text: false).in_batches.update_all(status_text: true)
-    CustomFilter.where(spoiler_text: false).in_batches.update_all(spoiler_text: true)
+    CustomFilter.where(spoiler: false).in_batches.update_all(spoiler: true)
   end
 end
