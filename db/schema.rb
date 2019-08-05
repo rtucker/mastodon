d9cb0d32ede7a93b3bc9e38fd7d5dd192629452f
multiple creatures <dev@multiple-creature.party>
2019-08-05 01:54:08 -0500

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_03_170051) do
+ActiveRecord::Schema.define(version: 2019_08_05_064643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
