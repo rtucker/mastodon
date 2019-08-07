72592b3c9c66b88fca117f38d4bddb662ecb641e
multiple creatures <dev@multiple-creature.party>
2019-08-07 01:51:02 -0500

    add distributability check to reblog worker

--- app/workers/reblog_status_worker.rb
+++ app/workers/reblog_status_worker.rb
@@ -8,7 +8,7 @@ class ReblogStatusWorker
   def perform(account_id, status_id, reblog_params = {})
     account = Account.find(account_id)
     status = Status.find(status_id)
-    return false if status.destroyed?
+    return false if status.destroyed? || !status.distributable?
     ReblogService.new.call(account, status, reblog_params.symbolize_keys)
     true
   rescue ActiveRecord::RecordNotFound, ActiveRecord::RecordInvalid
