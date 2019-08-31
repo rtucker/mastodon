a97ce6534d66c3500f5546f9bdbd5c7d2ed1f966
multiple creatures <dev@multiple-creature.party>
2019-08-30 19:33:25 -0500

    make sure id is an integer

--- app/workers/domain_block_worker.rb
+++ app/workers/domain_block_worker.rb
@@ -4,7 +4,7 @@ class DomainBlockWorker
   include Sidekiq::Worker
 
   def perform(domain_block_id)
-    domain_block = DomainBlock.find(domain_block_id)
+    domain_block = DomainBlock.find(domain_block_id.to_i)
     BlockDomainService.new.call(domain_block)
   rescue ActiveRecord::RecordNotFound
     true
