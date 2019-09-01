bff22832b742fb37d647bfd95f87756a4cced064
multiple creatures <dev@multiple-creature.party>
2019-09-01 05:33:03 -0500

    bump up authenticated media api req limit

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -61,7 +61,7 @@ class Rack::Attack
     req.remote_ip if req.api_request? && req.unauthenticated?
   end
 
-  throttle('throttle_api_media', limit: 30, period: 30.minutes) do |req|
+  throttle('throttle_api_media', limit: 300, period: 5.minutes) do |req|
     req.authenticated_user_id if req.post? && req.path.start_with?('/api/v1/media')
   end
 
