345ff4675ba79928a6de32399a577544607a12e7
multiple creatures <dev@multiple-creature.party>
2019-10-14 11:39:55 -0500

    exclude suspended (sub)domains from peers list

--- app/controllers/api/v1/instances/peers_controller.rb
+++ app/controllers/api/v1/instances/peers_controller.rb
@@ -7,10 +7,15 @@ class Api::V1::Instances::PeersController < Api::BaseController
   respond_to :json
 
   def index
-    render_cached_json('api:v1:instances:peers:index', expires_in: 1.day) { Account.remote.domains }
+    render_cached_json('api:v1:instances:peers:index', expires_in: 1.day) { actively_federated_domains }
   end
 
   private
+
+  def actively_federated_domains
+    blocks = DomainBlock.suspend
+    Account.remote.where(suspended_at: nil).domains.reject { |domain| blocks.where('domain LIKE ?', "%.#{domain}").exists? }
+  end
 
   def require_enabled_api!
     head 404 unless Setting.peers_api_enabled
