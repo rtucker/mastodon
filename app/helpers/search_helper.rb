91545d4ec4b081b6f0e99e013aa1bfdd9c930596
multiple creatures <dev@multiple-creature.party>
2019-12-10 03:26:09 -0600

    make query expander convert `heading: regex to match` shortcut into regex form

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -2,6 +2,13 @@ require 'sixarm_ruby_unaccent'
 
 module SearchHelper
 	def expand_search_query(query)
+    return '' if query.blank?
+    if query.include?(':')
+      query_parts = query.split(':', 2)
+      if query_parts[0].in?(%w(tags subj text desc))
+        query = "^#{query_parts[0]} .*#{query_parts[1]}"
+      end
+    end
     query.downcase.unaccent.gsub(/"(.*)"/, '\\y\1\\y')
   end
 end
