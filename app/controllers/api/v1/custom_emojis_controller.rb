503158b0cf2849e589a5d2728c591a7f0e65e00d
Holly "Frinkeldoodle" Lotor <admin@frinkel.tech>
2020-01-17 07:15:13 +0000

    Fix typo

--- app/controllers/api/v1/custom_emojis_controller.rb
+++ app/controllers/api/v1/custom_emojis_controller.rb
@@ -7,6 +7,6 @@ class Api::V1::CustomEmojisController < Api::BaseController
 
   def index
     expires_in 3.minutes, public: true
-    render_with_cache(each:serializer: REST:CustomEmojiSerializer) { CustomEmoji.local.where(disabled: false) }
+    render_with_cache(each_serializer: REST::CustomEmojiSerializer) { CustomEmoji.local.where(disabled: false) }
   end
 end
