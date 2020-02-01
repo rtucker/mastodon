rtucker/mastodon

commit fa8437f49adf8a6d8649102e557e3b341967c57f

fix replies not showing up

app/lib/status_filter.rb
@@ -13,7 +13,7 @@ class StatusFilter
   end
 
   def filtered?
-    return true if status.nil? || account.nil?
+    return true if status.nil?
     return false if !account.nil? && account.id == status.account_id
     return true if redis.sismember("filtered_statuses:#{account.id}", status.id)
     if blocked_by_policy? || (account_present? && filtered_status?) || silenced_account?

commit 0a5e08ebe765fe9b3d23b4aaef647368d8810b21

**MAJOR**: fix access policy short-circuit bug caused by `fa8437f49` & `cb38a5899` - **if you are running a monsterfork revision between `fa8437f49` and `389125d89` upgrade or apply this patch immediately!

app/lib/status_filter.rb
@@ -13,13 +13,15 @@ class StatusFilter
   end
 
   def filtered?
-    return true if status.nil?
-    return false if !account.nil? && account.id == status.account_id || account.nil?
+    return true if status.nil? || blocked_by_policy?
+    return false if account.nil? || account.id == status.account_id
     return true if redis.sismember("filtered_statuses:#{account.id}", status.id)
-    if blocked_by_policy? || (account_present? && filtered_status?) || silenced_account?
+
+    if filtered_status? || silenced_account?
       redis.sadd("filtered_statuses:#{account.id}", status.id)
       return true
     end
+
     false
   end
 