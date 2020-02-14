10c619500af6432b1f1c5f2856be31268a92a50b
multiple creatures <dev@multiple-creature.party>
2020-02-14 03:56:02 -0600

    mark new remote accounts known if graylist federation is disabled + auto-learn known accounts is enabled

--- app/services/activitypub/process_account_service.rb
+++ app/services/activitypub/process_account_service.rb
@@ -84,6 +84,7 @@ class ActivityPub::ProcessAccountService < BaseService
     @account.display_name            = @json['name'] || ''
     @account.note                    = @json['summary'] || ''
     @account.locked                  = @json['manuallyApprovesFollowers'] || false
+    @account.known                   = !Setting.auto_reject_unknown && Setting.auto_mark_known
     @account.froze                   = @json['froze'] || false
     @account.adult_content           = @json['adultContent'] || (@json['suggestedMinAge'].to_i >= 18)
     @account.gently                  = @json['gently'] || false
