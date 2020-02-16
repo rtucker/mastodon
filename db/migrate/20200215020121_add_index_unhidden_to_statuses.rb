1cea6f8f58202f56bf3cde27de38f549c4b1f0c2
multiple creatures <dev@multiple-creature.party>
2020-02-16 00:27:54 -0600

    remove unneeded `create index` migration

--- db/migrate/20200215020121_add_index_unhidden_to_statuses.rb
+++
@@ -1,7 +0,0 @@
-class AddIndexUnhiddenToStatuses < ActiveRecord::Migration[5.2]
-  disable_ddl_transaction!
-
-  def change
-    add_index :statuses, [:account_id, :id, :visibility], where: 'NOT hidden', algorithm: :concurrently, name: 'index_statuses_on_account_id_and_id_and_visibility_not_hidden'
-  end
-end
