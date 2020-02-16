511cfcc3b71ce26862d74d74c89891f3c8a35507
multiple creatures <dev@multiple-creature.party>
2020-02-16 00:25:40 -0600

    (monsterpit specific) make backfill redo-able

--- db/migrate/20200215021014_backfill_statuses_hidden.rb
+++ db/migrate/20200215021014_backfill_statuses_hidden.rb
@@ -1,8 +1,12 @@
 class BackfillStatusesHidden < ActiveRecord::Migration[5.2]
   disable_ddl_transaction!
 
-  def change
+  def up
     Rails.logger.info("Setting all statuses unhidden by default.  This may take a really long time.")
     Status.where.not(hidden: false).in_batches.update_all(hidden: false)
+  end
+
+  def down
+    true
   end
 end
