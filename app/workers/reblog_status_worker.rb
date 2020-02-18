4c1705e58dbdc47a90b9bc6d989d1a90ebd8287c
multiple creatures <dev@multiple-creature.party>
2020-02-17 19:34:35 -0600

    remove unique job constaint, handle `Mastodon::NotPermittedError`

--- app/workers/reblog_status_worker.rb
+++ app/workers/reblog_status_worker.rb
@@ -3,15 +3,13 @@
 class ReblogStatusWorker
   include Sidekiq::Worker
 
-  sidekiq_options unique: :until_executed
-
   def perform(account_id, status_id, reblog_params = {})
     account = Account.find(account_id)
     status = Status.find(status_id)
     return false if status.destroyed? || !status.distributable?
     ReblogService.new.call(account, status, reblog_params.symbolize_keys)
     true
-  rescue ActiveRecord::RecordNotFound, ActiveRecord::RecordInvalid
+  rescue ActiveRecord::RecordNotFound, ActiveRecord::RecordInvalid, Mastodon::NotPermittedError
     true
   end
 end
