b3bde7bdc32806add6bf073403cf31f0872ea338
multiple creatures <dev@multiple-creature.party>
2020-02-21 04:22:59 -0600

    re-add missing max description length from env var

--- app/models/media_attachment.rb
+++ app/models/media_attachment.rb
@@ -26,6 +26,8 @@ class MediaAttachment < ApplicationRecord
 
   enum type: [:image, :gifv, :video, :unknown, :audio]
 
+  MAX_DESCRIPTION_LENGTH = (ENV['MAX_DESCRIPTION_LENGTH'] || 6666).to_i
+
   IMAGE_FILE_EXTENSIONS = %w(.jpg .jpeg .png .gif).freeze
   VIDEO_FILE_EXTENSIONS = %w(.webm .mp4 .m4v .mov).freeze
   AUDIO_FILE_EXTENSIONS = %w(.ogg .oga .mp3 .wav .flac .opus .aac .m4a .3gp .wma).freeze
