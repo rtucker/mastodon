c32c3f70cdf4e9f113d24e2d9b875fe7052bc839
multiple creatures <dev@multiple-creature.party>
2020-02-22 00:26:55 -0600

    fix auto-defang....... 2!

--- app/workers/scheduler/defang_scheduler.rb
+++ app/workers/scheduler/defang_scheduler.rb
@@ -5,8 +5,8 @@ class Scheduler::DefangScheduler
   include ServiceAccountHelper
 
   def perform
-    User.where(defanged: false, last_fanged_at: nil).or(User.where('last_fanged_at <= ?', 15.minutes.ago)) do
-      |user| user.defang!
+    User.where(defanged: false, last_fanged_at: nil).or(User.where('last_fanged_at <= ?', 15.minutes.ago)) do |user|
+      user.defang!
       next unless user&.account.present?
       service_dm('announcements', user.account, "You are no longer in #{user.role} mode.", footer: 'auto-defang')
     end
