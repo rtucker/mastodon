19e7b163103b1a5dc97182aadf72c2753b98b5bf
multiple creatures <dev@multiple-creature.party>
2020-02-22 18:18:29 -0600

    add missing `find_each`

--- app/workers/scheduler/defang_scheduler.rb
+++ app/workers/scheduler/defang_scheduler.rb
@@ -5,9 +5,10 @@ class Scheduler::DefangScheduler
   include ServiceAccountHelper
 
   def perform
-    User.where(defanged: false, last_fanged_at: nil).or(User.where('last_fanged_at <= ?', 15.minutes.ago)) do |user|
+    User.where(defanged: false, last_fanged_at: nil).or(User.where('last_fanged_at <= ?', 15.minutes.ago)).find_each do |user|
       user.defang!
       next unless user&.account.present?
+
       service_dm('announcements', user.account, "You are no longer in #{user.role} mode.", footer: 'auto-defang')
     end
   end
