416d51727ddb90c722df8587e5f2896722477fae
multiple creatures <dev@multiple-creature.party>
2020-03-01 23:15:37 -0600

    fix `index unique` for custom filters - should be on `[:account_id, :phrase]` instead of just `[:phrase]`

--- db/migrate/20200227214441_add_more_indexes.rb
+++ db/migrate/20200227214441_add_more_indexes.rb
@@ -1,8 +1,8 @@
 class AddMoreIndexes < ActiveRecord::Migration[5.2]
-  def up
+  def change
     safety_assured do
       add_index :domain_blocks, :severity
-      add_index :custom_filters, :phrase, unique: true
+      add_index :custom_filters, [:account_id, :phrase], unique: true
     end
   end
 end
